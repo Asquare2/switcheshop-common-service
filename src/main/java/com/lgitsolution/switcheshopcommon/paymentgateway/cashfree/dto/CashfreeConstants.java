8ba26a1b64a678c31c7c78bc348a933fce92e5a4
Dell <Dell@DESKTOP-8C2QUP0>
2024-09-11 23:49:22 +0530

    Nikhil: Cashfree payment status PAID constant.

--- src/main/java/com/lgitsolution/switcheshopcommon/paymentgateway/cashfree/dto/CashfreeConstants.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/paymentgateway/cashfree/dto/CashfreeConstants.java
@@ -3,8 +3,11 @@ package com.lgitsolution.switcheshopcommon.paymentgateway.cashfree.dto;
 
 public class CashfreeConstants {
 
-  /** The constant of the PAID status of cashfree payment order. */
+  /** The constant of the ACTIVE status of cashfree payment order. */
   public static final String CASHFREE_ORDER_STATUS_ACTIVE = "ACTIVE";
+
+  /** The constant of the PAID status of cashfree payment order. */
+  public static final String CASHFREE_ORDER_STATUS_PAID = "PAID";
 
   /** The constant of the currency type indian rupee. */
   public static final String CASHFREE_CURRENCY_TYPE_INDIAN_RUPEE = "INR";
