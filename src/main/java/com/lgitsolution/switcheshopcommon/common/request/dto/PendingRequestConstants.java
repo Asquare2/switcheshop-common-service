04a56a6c20567a73f176d3db8086b58f5bf1d98f
Dell <Dell@DESKTOP-8C2QUP0>
2024-09-13 00:21:09 +0530

    Nikhil: Cashfree pending request scheduler code shift at the admin portal.

---
+++ src/main/java/com/lgitsolution/switcheshopcommon/common/request/dto/PendingRequestConstants.java
@@ -0,0 +1,8 @@
+
+package com.lgitsolution.switcheshopcommon.common.request.dto;
+
+public class PendingRequestConstants {
+
+  public static final String PR_VERIFY_CASHFREE_PAYMENT_STATUS = "VERIFY_CASHFREE_PAYMENT_STATUS";
+
+}
