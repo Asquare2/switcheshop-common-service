01a7453e551921d3e40560ac3c5238ece80deb70
Nikhil Soni <nsoni755@gmail.com>
2024-12-05 21:50:06 +0530

    Nikhil: Add a promocode constant class.

---
+++ src/main/java/com/lgitsolution/switcheshopcommon/promocode/dto/PromocodeConstants.java
@@ -0,0 +1,12 @@
+
+package com.lgitsolution.switcheshopcommon.promocode.dto;
+
+public class PromocodeConstants {
+
+  /** The constants holds the percentage discount. */
+  public static final int PC_DISCOUNT_AMOUNT_IN_PERCENTAGE = 0;
+
+  /** The constants holds the percentage discount. */
+  public static final int PC_DISCOUNT_AMOUNT_IN_AMOUNT = 1;
+
+}
