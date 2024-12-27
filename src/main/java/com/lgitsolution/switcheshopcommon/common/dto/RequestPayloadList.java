f090517a6abc3180b8318522e312c16b37727c7b
aaash <aaash@DESKTOP-HKLOHVN>
2024-12-27 16:27:31 +0530

    add request payload list

---
+++ src/main/java/com/lgitsolution/switcheshopcommon/common/dto/RequestPayloadList.java
@@ -0,0 +1,16 @@
+
+package com.lgitsolution.switcheshopcommon.common.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RequestPayloadList {
+  private List<Object> list;
+}
