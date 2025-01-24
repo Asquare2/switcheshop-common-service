c21c415322a15111f7ef2df487673ffe0b72d8f7
Ashish Agrawal <aaashish456@gmail.com>
2025-01-24 22:40:54 +0530

    bug fix

--- src/main/java/com/lgitsolution/switcheshopcommon/invoice/utility/Utility.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/invoice/utility/Utility.java
@@ -4,6 +4,8 @@ package com.lgitsolution.switcheshopcommon.invoice.utility;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lgitsolution.switcheshopcommon.invoice.dto.InvoiceDetailsDto;
 import com.lgitsolution.switcheshopcommon.invoice.dto.InvoiceItemDto;
 import com.lgitsolution.switcheshopcommon.invoice.model.InvoiceDetails;
@@ -33,8 +35,7 @@ public class Utility {
     dto.setTotalPayable(model.getTotalPayable());
     dto.setTaxTotalAmount(model.getTaxTotalAmount());
     dto.setInvoiceNumber(model.getInvoiceNumber());
-    dto.setInvoiceItemsDetailList(CommonUtility.convertJsonToObject(model
-            .getInvoiceItemsDetailList(), new ArrayList<InvoiceItemDto>()));
+    dto.setInvoiceItemsDetailList(parseJsonToList(model.getInvoiceItemsDetailList()));
     return dto;
   }
 
@@ -45,5 +46,22 @@ public class Utility {
    */
   public static List<InvoiceDetailsDto> convertModelToDto(List<InvoiceDetails> modelList) {
     return modelList.stream().map(model -> convertModelToDto(model)).toList();
+  }
+
+  /**
+   * Parse json string to address details list.
+   * 
+   * @param json string
+   * @return the list of address details
+   */
+  private static List<InvoiceItemDto> parseJsonToList(String json) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      return objectMapper.readValue(json, new TypeReference<ArrayList<InvoiceItemDto>>() {
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException("Failed to parse JSON to List<CustomerDetailsDt0>", e);
+    }
   }
 }
