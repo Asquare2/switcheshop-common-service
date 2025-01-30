59e72d77d642ca5d2772f6ba55334a229d1f54d1
Ashish Agrawal <aaashish456@gmail.com>
2025-01-30 14:45:56 +0530

    Return Changes

--- src/main/java/com/lgitsolution/switcheshopcommon/common/request/dto/PendingRequestConstants.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/common/request/dto/PendingRequestConstants.java
@@ -7,6 +7,8 @@ public class PendingRequestConstants {
 
   public static final String PR_ORDER_TRACK_STATUS = "ORDER_TRACK";
 
+  public static final String PR_RETURN_ORDER_TRACK_STATUS = "RETURN_ORDER_TRACK";
+
   public static final int PR_VERIFY_CASHFREE_PAYMENT_MAX_RUN_COUNT = 50;
 
   public static final int PR_ORDER_TRACK_STATUS_MAX_RUN_COUNT = 30;
