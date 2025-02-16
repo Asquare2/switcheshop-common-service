1f269cd52fa0f32aea8886d7ffeb9b3eef697ea6
Ashish Agrawal <aaashish456@gmail.com>
2025-02-16 14:23:38 +0530

    image changes

--- src/main/java/com/lgitsolution/switcheshopcommon/featuresection/dto/FeatureSectionType1Dto.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/featuresection/dto/FeatureSectionType1Dto.java
@@ -3,7 +3,6 @@ package com.lgitsolution.switcheshopcommon.featuresection.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.lgitsolution.switcheshopcommon.common.dto.Multimedia;
 import com.lgitsolution.switcheshopcommon.filter.dto.SearchOptionsDto;
 
 import lombok.Data;
@@ -14,7 +13,9 @@ import lombok.Data;
 public class FeatureSectionType1Dto {
 
   /** This is multi media comes from User interface. */
-  private Multimedia multiMedia;
+  private String multiMedia;
+
+  private String imageAltTag;
 
   private SearchOptionsDto searchOptionsDto;
 
