6502de9a85fc7d338e19fc3e8c8093f0cf537d91
Ashish Agrawal <aaashish456@gmail.com>
2025-03-05 00:29:41 +0530

    Remove some unwanted columns

--- src/main/java/com/lgitsolution/switcheshopcommon/order/dto/OrderItemsDto.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/order/dto/OrderItemsDto.java
@@ -14,16 +14,12 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OrderItemsDto {
 
-  public static final String ID = "id";
-  public static final String ORDER_DETAIL_ID = "orderDetailId";
-  public static final String SKU_ID = "skuId";
   public static final String QUANTITY = "quantity";
   public static final String IDENTIFICATION_NUMBER = "identificationNumber";
   public static final String SKU_NAME = "skuName";
   public static final String TITLE = "title";
   public static final String ORIGINAL_PRICE = "originalPrice";
   public static final String SELLING_PRICE = "sellingPrice";
-  public static final String SALE_ID = "saleId";
   public static final String HSN_CODE = "hsnCode";
 
   private Integer id;
--- src/main/java/com/lgitsolution/switcheshopcommon/sku/dto/SKUDto.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/sku/dto/SKUDto.java
@@ -12,28 +12,13 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SKUDto {
 
-  public static final String ID = "id";
   public static final String NAME = "name";
-  public static final String PRODUCT_ID = "productId";
   public static final String QUANTITY = "quantity";
   public static final String PRICE = "price";
   public static final String SPECIAL_PRICE = "specialPrice";
-  public static final String ATTRIBUTE_OPTIONS = "attributeOptions";
-  public static final String TITLE = "title";
-  public static final String SHORT_DESCRIPTION = "shortDescription";
-  public static final String LONG_DESCRIPTION = "longDescription";
-  public static final String SLUG = "slug";
-  public static final String DM_CONTENT = "dmContent";
   public static final String ITEM_ID = "itemId";
-  public static final String SALE_PRICE = "salePrice";
-  public static final String DISCOUNT_PERCENTAGE = "discountPercentage";
   public static final String AVERAGE_RATING = "averageRating";
-  public static final String FLASH_SALE_ID = "flashSaleId";
-  public static final String MINIMUM_ORDER_QUANTITY = "minimumOrderQuantity";
-  public static final String MAXIMUM_ORDER_QUANTITY = "maximumOrderQuantity";
   public static final String PRODUCT_IDENTIFICATION_NO = "productIdentificationNo";
-  public static final String CATEGORY_ID = "categoryId";
-  public static final String SEARCH_VALUES = "searchValues";
 
   private Integer id;
 
