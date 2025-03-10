9853f56e57f1ae4a48754a84a57affbe2c20fed6
Ashish Agrawal <aaashish456@gmail.com>
2025-03-10 18:48:26 +0530

    new list in status

--- src/main/java/com/lgitsolution/switcheshopcommon/order/dto/OrderStatusDetail.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/order/dto/OrderStatusDetail.java
@@ -1,6 +1,8 @@
 
 package com.lgitsolution.switcheshopcommon.order.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
@@ -16,6 +18,10 @@ public class OrderStatusDetail {
   private int count = 0;
 
   private int statusCode = -1;
+
+  // this list we are creating because if any additional code then we can put here example :
+  // Paid_Order_Cancelled_By_Customer(16)
+  private List<Integer> additionalStatusCodeList = null;
 
   private int id;
 }
