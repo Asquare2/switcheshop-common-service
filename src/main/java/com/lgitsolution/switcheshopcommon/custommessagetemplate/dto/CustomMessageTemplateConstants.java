151fbcec779b23b9d751294101ae29cc260bedd7
Nikhil Soni <nsoni755@gmail.com>
2025-03-15 19:03:45 +0530

    Nikhil: Custom message related updates.

--- src/main/java/com/lgitsolution/switcheshopcommon/custommessagetemplate/dto/CustomMessageTemplateConstants.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/custommessagetemplate/dto/CustomMessageTemplateConstants.java
@@ -16,6 +16,24 @@ public class CustomMessageTemplateConstants {
   /** The constant holds the password variable. */
   public static final String CMT_PASSWORD_VAR = "<PASSWORD>";
 
+  /** The constant holds the order id variable. */
+  public static final String ORDER_ID_VAR = "<ORDER_ID>";
+
+  /** The constant holds the product names variable. */
+  public static final String PRODUCT_NAMES_VAR = "<PRODUCT_NAMES>";
+
+  /** The constant holds the company name variable. */
+  public static final String COMPANY_NAME_VAR = "<COMPANY_NAME>";
+
+  /** The constant holds the carrier name variable. */
+  public static final String CARRIER_NAME_VAR = "<CARRIER_NAME>";
+
+  /** The constant holds the shipping id variable. */
+  public static final String SHIPPING_ID_VAR = "<SHIPPING_ID>";
+
+  /** The constant holds the link variable. */
+  public static final String LINK_VAR = "<LINK>";
+
   /* Message template types constants */
   /** The constant holds the otp email message type value. */
   public static final String CMT_OTP_EMAIL_MSG_TYPE = "Otp Email";
@@ -25,5 +43,14 @@ public class CustomMessageTemplateConstants {
 
   /** The constant holds the master admin password email message type value. */
   public static final String CMT_MADMIN_PASSWORD_EMAIL_MSG_TYPE = "Master Admin Password Email";
+
+  /** The constant holds the order confirmed email message type value. */
+  public static final String CMT_ORDER_CONFIRMED_MSG_TYPE = "Order Confirmed";
+
+  /** The constant holds the order shipped email message type value. */
+  public static final String CMT_ORDER_SHIPPED_MSG_TYPE = "Order Shipped";
+
+  /** The constant holds the order delivered email message type value. */
+  public static final String CMT_ORDER_DELIVERED_MSG_TYPE = "Order Delivered";
 
 }
