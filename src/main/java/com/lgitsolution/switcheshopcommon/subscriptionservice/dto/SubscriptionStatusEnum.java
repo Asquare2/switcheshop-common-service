ff73f1728e65d4cbdaa7380700e646ac970898ec
Ashish Agrawal <aaashish456@gmail.com>
2025-05-11 01:35:18 +0530

    new status

--- src/main/java/com/lgitsolution/switcheshopcommon/subscriptionservice/dto/SubscriptionStatusEnum.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/subscriptionservice/dto/SubscriptionStatusEnum.java
@@ -3,4 +3,7 @@ package com.lgitsolution.switcheshopcommon.subscriptionservice.dto;
 
 public enum SubscriptionStatusEnum {
   free, active, expired, prepaid, paused, inprogress, failed
+
+  // below status is only for calling by payment service
+  , success
 }
