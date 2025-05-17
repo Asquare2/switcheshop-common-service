13c47e2763afb2cd7e4fd5ecc461a309b16b815e
HP <HP@Nikhil>
2025-05-17 22:15:13 +0530

    Nikhil: Subscription success email related changes part 2.

---
+++ src/main/java/com/lgitsolution/switcheshopcommon/custommessagetemplate/model/SubscriptionCustomMessageTemplate.java
@@ -0,0 +1,49 @@
+
+package com.lgitsolution.switcheshopcommon.custommessagetemplate.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ * The custom message template model class to connect with database.
+ */
+@Data
+@Entity
+@Table(name = "subscription_custom_message_template", schema = "public")
+public class SubscriptionCustomMessageTemplate {
+
+  /** The id. */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  /** The types of the messages. */
+  @Column(name = "types")
+  private String types;
+
+  /** The default title of the message. */
+  @Column(name = "default_title")
+  private String defaultTitle;
+
+  /** The default message. */
+  @Column(name = "default_message")
+  private String defaultMessage;
+
+  /** The custom title of the message. */
+  @Column(name = "custom_title")
+  private String customTitle;
+
+  /** The custom title of the message. */
+  @Column(name = "custom_message")
+  private String customMessage;
+
+  /** The email type for which the template will be send as email. */
+  @Column(name = "email_type")
+  private String emailType;
+
+}
--- src/main/java/com/lgitsolution/switcheshopcommon/custommessagetemplate/utility/Utility.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/custommessagetemplate/utility/Utility.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import com.lgitsolution.switcheshopcommon.custommessagetemplate.dto.CustomMessageTemplateDto;
 import com.lgitsolution.switcheshopcommon.custommessagetemplate.model.CustomMessageTemplate;
+import com.lgitsolution.switcheshopcommon.custommessagetemplate.model.SubscriptionCustomMessageTemplate;
 
 import io.micrometer.common.util.StringUtils;
 
@@ -49,6 +50,47 @@ public class Utility {
     List<CustomMessageTemplateDto> customMessageTemplateDtoList = new ArrayList<>();
     customMessageTemplateList.stream().forEach(customMessageTemplate -> customMessageTemplateDtoList
             .add(convertModelToDto(customMessageTemplate)));
+    return customMessageTemplateDtoList;
+  }
+
+  /**
+   * Converts the model to dto object.
+   * 
+   * @param customMessageTemplate the custom message template
+   * @return the custom message template dto object
+   */
+  public static CustomMessageTemplateDto convertSubscriptionModelToDto(
+          SubscriptionCustomMessageTemplate customMessageTemplate) {
+    CustomMessageTemplateDto customMessageTemplateDto = new CustomMessageTemplateDto();
+    customMessageTemplateDto.setId(customMessageTemplate.getId());
+    customMessageTemplateDto.setTypes(customMessageTemplate.getTypes());
+    String customTitle = customMessageTemplate.getCustomTitle();
+    if (customTitle != null && StringUtils.isNotEmpty(customTitle)) {
+      customMessageTemplateDto.setCustomTitle(customTitle);
+    } else {
+      customMessageTemplateDto.setCustomTitle(customMessageTemplate.getDefaultTitle());
+    }
+    String customMessage = customMessageTemplate.getCustomMessage();
+    if (customMessage != null && StringUtils.isNotEmpty(customMessage)) {
+      customMessageTemplateDto.setCustomMessage(customMessage);
+    } else {
+      customMessageTemplateDto.setCustomMessage(customMessageTemplate.getDefaultMessage());
+    }
+    customMessageTemplateDto.setEmailType(customMessageTemplate.getEmailType());
+    return customMessageTemplateDto;
+  }
+
+  /**
+   * Converts the list of model object to list of dto object.
+   * 
+   * @param customMessageTemplateList the list of model object
+   * @return the list of dto object
+   */
+  public static List<CustomMessageTemplateDto> convertSubscriptionModelToDto(
+          List<SubscriptionCustomMessageTemplate> customMessageTemplateList) {
+    List<CustomMessageTemplateDto> customMessageTemplateDtoList = new ArrayList<>();
+    customMessageTemplateList.stream().forEach(customMessageTemplate -> customMessageTemplateDtoList
+            .add(convertSubscriptionModelToDto(customMessageTemplate)));
     return customMessageTemplateDtoList;
   }
 
