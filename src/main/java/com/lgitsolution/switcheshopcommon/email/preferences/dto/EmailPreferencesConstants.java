6cdd80b4c5f104f5c7ae06a0a2a3f79584aa3757
HP <HP@Nikhil>
2025-05-25 18:29:27 +0530

    Nikhil: Personal name related update.

--- src/main/java/com/lgitsolution/switcheshopcommon/email/preferences/dto/EmailPreferencesConstants.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/email/preferences/dto/EmailPreferencesConstants.java
@@ -6,4 +6,7 @@ public class EmailPreferencesConstants {
   /** The constant for the otp email type. */
   public static final String EPC_OTP_EMAIL_TYPE = "otpEmail";
 
+  /** The constant for the personal name key. */
+  public static final String EC_PERSONAL_NAME_KEY = "personalName";
+
 }
