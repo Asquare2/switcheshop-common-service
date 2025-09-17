e1181c5fffe2cda338b9ec5f71c4e523a4062f8b
Ashish Agrawal <aaashish456@gmail.com>
2025-09-18 00:05:37 +0530

    ADD TRENDING SEARCH COLUMN

--- src/main/java/com/lgitsolution/switcheshopcommon/common/dto/CommonConstants.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/common/dto/CommonConstants.java
@@ -130,6 +130,8 @@ public class CommonConstants {
 
   public static final String MIS_SETTING_PAYMENT_GATEWAY_NAME_KEY = "payment_gateway_name";
 
+  public static final String MIS_SETTING_TRENDING_SEARCH_NAME_KEY = "trending_search";
+
   // Miscellaneous settings constant end
 
   // search operations name start
