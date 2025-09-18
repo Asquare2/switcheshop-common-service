80604d22b056e23ab3df0c24469f14b12bf24354
Ashish Agrawal <aaashish456@gmail.com>
2025-09-18 23:00:31 +0530

    add new constant

--- src/main/java/com/lgitsolution/switcheshopcommon/common/dto/CommonConstants.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/common/dto/CommonConstants.java
@@ -130,7 +130,7 @@ public class CommonConstants {
 
   public static final String MIS_SETTING_PAYMENT_GATEWAY_NAME_KEY = "payment_gateway_name";
 
-  public static final String MIS_SETTING_TRENDING_SEARCH_NAME_KEY = "trending_search";
+  public static final String MIS_SETTING_TRENDING_SEARCH_NAME_KEY = "poluler_search";
 
   // Miscellaneous settings constant end
 
