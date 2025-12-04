502f7600b57b955c82e6d1990092dcc1b985c8e4
HP <HP@Nikhil>
2025-12-04 22:58:35 +0530

    Nikhil: Barcode label related changes.

--- src/main/java/com/lgitsolution/switcheshopcommon/common/util/Utility.java
+++ src/main/java/com/lgitsolution/switcheshopcommon/common/util/Utility.java
@@ -17,6 +17,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,9 +37,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.lgitsolution.switcheshopcommon.common.dto.ModuleAccess;
 import com.lgitsolution.switcheshopcommon.common.dto.Multimedia;
 import com.lgitsolution.switcheshopcommon.common.dto.ResponseWrapper;
@@ -530,6 +535,19 @@ public class Utility {
       logger.error("Unable to generate barcode: " + e);
     }
     return MatrixToImageWriter.toBufferedImage(bitMatrix);
+  }
+
+  // Generate a barcode image (1D or 2D)
+  public static BufferedImage generateBarcode(String data, BarcodeFormat format, int width,
+          int height) throws WriterException {
+    Map<EncodeHintType, Object> hints = new HashMap<>();
+    if (format == BarcodeFormat.QR_CODE) {
+      hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+    }
+    hints.put(EncodeHintType.MARGIN, 1);
+
+    BitMatrix matrix = new MultiFormatWriter().encode(data, format, width, height, hints);
+    return MatrixToImageWriter.toBufferedImage(matrix);
   }
 
   /**
